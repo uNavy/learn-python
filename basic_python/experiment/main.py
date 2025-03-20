bff8798a0d392023ffbd23740f69638dc4faa534
JuanLie13 <JuanLIE@rjk.raffles-entrepreneur.com>
2025-03-20 22:12:48 +0700

    freareg

--- basic_python/experiment/main.py
+++ basic_python/experiment/main.py
@@ -9,6 +9,6 @@ def sum_then_print(*numbers):
         print("Current total: ", total)
     return(total)
 
-hasil = sum_then_print(2, 3, 4, 5, 4)
-print(hasil)
+result = sum_then_print(2, 3, 4, 5, 4)
+print(result)
 # output ➜ 18
